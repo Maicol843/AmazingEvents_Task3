a0be21df95b5606816b18f6def08833cdc7f9723
Maicol <mdmch13@gmail.com>
2023-03-02 11:22:10 -0300

    Se modificaron los html y los js, ademas se creo los demas js que faltaban. Se agregaron todos los eventos manipulados desde JavaScript

---
+++ stats.js
@@ -0,0 +1,8 @@
+const botonHome = document.getElementById('botonHome', 'botonUpcoming', 'botonPast', 'botonContact', 
+'botonStats')
+
+botonHome.addEventListener('click',mensaje);
+botonUpcoming.addEventListener('click',mensaje);
+botonPast.addEventListener('click',mensaje);
+botonContact.addEventListener('click',mensaje);
+botonStats.addEventListener('click',mensaje);
